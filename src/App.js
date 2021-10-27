f9b4dc8520e94dc8f498b8d907fbc91b8e5581b8
sarankumarsritharan <79304959+sarankumarsritharan@users.noreply.github.com>
2021-10-27 08:57:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function form() {
+  return( <div>
+    <h1>Job Application Form Using Formik</h1>
+    <Formik
+      initialValues={{
+        candidateName: '',
+        emailId: '',
+        gender: '',
+        roleAppliedFor: '',
+        terms: ''
+      }}
+
+      validate={values => {
+        const errors = {};
+
+        if (!values.candidateName) {
+          errors.candidateName = 'Candidate Name is required';
+        }
+
+        if (!values.emailId) {
+          errors.emailId = 'Email is required';
+        }else if (!!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.emailId)) {
+          errors.emailId = 'Email is invalid';
+        }
+
+        if (!values.gender) {
+          errors.gender = 'Gender is required';
+        }
+        
+        if(!values.roleAppliedFor){
+          errors.roleAppliedFor = 'Role is required';
+        }
+
+        if(!values.terms){
+          errors.terms = 'Terms check is required';
+        }
+        
+
+        return errors;
+      }}
+      onSubmit={values => {
+        console.log(values);
+        console.log('Form Submitted');
+      }}
+    >
+      {formik => {
+        return (
+          <Form>
+           
+            <label>Candidate Name : </label>
+            <Field type="text" name="candidateName" />
+            {" "}{" "}{" "}
+            
+            <ErrorMessage name="candidateName" />
+            <br />
+            <br />
+            <label>Email Id : </label>
+            <Field type="email" name="emailId" />
+            {" "}
+            {" "}
+            {" "}
+            
+            <ErrorMessage name="emailId" />
+            <br />
+            <br />
+            <label>Gender:</label>
+            <label><Field type="radio" name="picked" value="male" />Male</label>
+            <label><Field type="radio" name="picked" value="female" />Female</label>
+            <label><Field type="radio" name="picked" value="others" />Others</label>
+            {" "}
+            {" "}
+            {" "}
+            
+            <ErrorMessage name="gender"/>
+            <br/>
+            <br/>
+            <label>Role Applied For: </label>
+            <select>
+            {/* <Field type="dropdown">  */}
+              <option value="" label="Select a role"> Role {" "}</option>
+              <option value="Fullstack Developer" label="Fullstack Developer"> {" "}Fullstack Developer </option>
+              <option value="JS Developer" label="JS Developer"> {" "}JS Developer </option>
+              <option value="Front End Developer" label="Front End Developer"> {" "}Front End Developer </option>
+              <option value="Back End Developer" label="Back End Developer"> {" "}Back End Developer </option>
+            </select>
+              {/* </Field> */}
+            {" "}
+            {" "}
+            {" "}
+            
+            <ErrorMessage name="roleAppliedFor"/>
+            <br/>
+            <br/>
+            <label>Terms :</label>
+            <label><Field type="Checkbox" name="checked" value="terms"/>Accept the terms and conditions</label>
+            {" "}
+            {" "}
+            {" "}
+            
+            <ErrorMessage name="terms"/>
+            <br/>
+            <br/>
+            <button type="submit">Submit</button>
+            
+          </Form>
+        );
+      }}
+    </Formik>
+  
+  </div>
+  
+);
 }
 
-export default App;
+
+
+
+ 
